b84b94e711c97061d179a6e4269cdecf84abbb7c
nawodyaishan <nawodyain@gmail.com>
2022-07-15 00:57:19 +0530

    Add SO Props

--- Assets/Data/WaveConfigSo.cs
+++ Assets/Data/WaveConfigSo.cs
@@ -1,9 +1,35 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Data
 {
+    [CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
     public class WaveConfigSo : ScriptableObject
     {
-        
+        [SerializeField] Transform pathPrefab;
+        [SerializeField] float moveSpeed;
+
+        public Transform GetStartingWaypoint()
+        {
+            // Returning Starting Waypoint
+            return pathPrefab.GetChild(0);
+        }
+
+        public List<Transform> GetWaypoints()
+        {
+            List<Transform> waypoints = new List<Transform>();
+
+            foreach (var child in waypoints)
+            {
+                waypoints.Add(child);
+            }
+
+            return waypoints;
+        }
+
+        public float GetMoveSpeed()
+        {
+            return this.moveSpeed;
+        }
     }
 }
